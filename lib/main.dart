b1528d63812d5769ed76d733d1c8c122e85bd4cd
Anushka-shukla <shuklaannushka@gmail.com>
2020-05-20 02:52:14 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/screens/notes_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class  MyApp extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:"Notes",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: NoteList(),
+
+    );
+
+
+  }
+}
