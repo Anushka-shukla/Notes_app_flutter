7b617eeac3a57db40aba0608dec800d8b85cf235
viralvaghela <viralvaghela7109@gmail.com>
2020-10-01 16:11:45 +0530

    Dark mode Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:notes_app/screens/notes_list.dart';
 
@@ -5,19 +6,22 @@ void main() {
   runApp(MyApp());
 }
 
-class  MyApp extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title:"Notes",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: NoteList(),
-
-    );
-
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => new ThemeData(
+              primarySwatch: Colors.purple,
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return new MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            theme: theme,
+            home: NoteList(),
+          );
+        });
   }
 }
